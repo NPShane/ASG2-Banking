43a3c9dd05208c3fb6b2fe6a07efa2457f1782bd
NPShane <s10239824@connect.np.edu.sg>
2023-02-08 19:38:07 +0800

    Redirect to login added index now redirects to login happens if user is not logged in (no data in localStorage)

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const bar = document.getElementById('bar');
-const nav = document.getElementById('navbar');
-const close = document.getElementById('close')
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close");
 
-if(bar)
-{
-    bar.addEventListener('click',()=>{
-        nav.classList.add('active');
-    })
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
 }
 
-if(close)
-{
-    close.addEventListener('click',()=>
-    {
-        nav.classList.remove('active');   
-    })
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
+if (localStorage.getItem("username") == null) {
+  console.log("Not logged in");
+  location.replace("./login.html");
 }
